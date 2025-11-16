c142104779b280559147e7844637d050c760e195
siddharthsoni501 <siddharthsoni75480@gmail.com>
2025-11-16 23:32:26 +0530

    Add files via upload

---
+++ program to find area of circle.py
@@ -0,0 +1,9 @@
+# Program to calculate area of a circle
+
+# Taking input from user
+radius = float(input("Enter the radius of the circle: "))
+
+# Formula for area of circle = π * r^2
+area = 3.14159 * (radius ** 2)
+
+print("The area of the circle is:", area)
